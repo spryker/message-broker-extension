f975995b19cf4b1faaf7d8fdfbecbbc6fb8b574e
stereomon <rene.klatt@spryker.com>
2022-01-21 12:54:11 +0100

    APPS-1393 Fixed typo in namespace.

--- src/Spryker/Zed/MessageBrokerExtension/Dependency/Plugin/MessageSenderPluginInterface.php
+++ src/Spryker/Zed/MessageBrokerExtension/Dependency/Plugin/MessageSenderPluginInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\MessageBrokerExtension\Dependecy\Plugin;
+namespace Spryker\Zed\MessageBrokerExtension\Dependency\Plugin;
 
 use Symfony\Component\Messenger\Transport\Sender\SenderInterface;
 
